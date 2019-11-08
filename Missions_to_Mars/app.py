2eba038f6bddb893322eef261c5e27a67c6124d5
Mary Brown <maryadbrown@yahoo.com>
2019-11-08 18:52:17 -0500

    debug app.py, move flash init to begin of app

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -4,16 +4,13 @@ from flask_table import Table, Col
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-
-
 # create instance of Flask app
 app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
 
 @app.route("/")
 def index():
